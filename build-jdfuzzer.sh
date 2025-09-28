93c8bc064f0f3467ee02b878abd47df3904d0d4a
JDozerFuzzer <jdozerfuzzer@gmail.com>
2025-09-27 22:27:08 -0300

    Update builder file

--- build-jdfuzzer.sh
+++ build-jdfuzzer.sh
@@ -3,12 +3,13 @@
 local_home=$(pwd)
 
 jdfuzzer_version=0.9
+jdozerfuzzer=jdozerfuzzer/jdozer-fuzzer-standalone:${jdfuzzer_version}
 
-docker stop jdfuzzer-std || true
+sudo docker stop jdfuzzer-std || true
 
-docker rm jdfuzzer-std || true
+sudo docker rm jdfuzzer-std || true
 
-docker image rm jdozer-fuzzer-standalone:${jdfuzzer_version} || true
+sudo docker image rm ${jdozerfuzzer} || true
 
 #################################################################################################
 
@@ -17,15 +18,12 @@ vectors_home=../jdozer-fuzzer-vectors
 engine_home=../jdozer-fuzzer-engine
 processor_home=../jdozer-fuzzer-processor
 
-#cd $vectors_home
-#mvn dependency:resolve
-#pwd
-#quarkus build --no-codegen --no-test 
-# --output target/uberjar/jdozer-fuzzer-vectors.jar
-# quarkus build --no-codegen --no-test --output target/uberjar/jdozer-fuzzer-vectors.jar
-#cd $local_home
+cd $vectors_home
+mvn dependency:resolve
+quarkus build
+cd $local_home
 
-docker build -t jdozer-fuzzer-standalone:${jdfuzzer_version} -f Dockerfile-standalone ../
-docker container create -p 8080:8080 -p 6379:6379 --name jdfuzzer-std jdozer-fuzzer-standalone:${jdfuzzer_version}
-docker start jdfuzzer-std
+sudo docker build -t ${jdozerfuzzer} -f Dockerfile-standalone ../
+sudo docker container create -p 8080:8080 -p 6379:6379 --name jdfuzzer-std ${jdozerfuzzer}
+sudo docker start jdfuzzer-std
 
